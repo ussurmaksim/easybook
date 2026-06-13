f7078c156b0f0a9f206623a8f3d76e66f349dbd2
ussurmaksim <94112489+ussurmaksim@users.noreply.github.com>
2026-06-13 23:17:41 +0300

    fix middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,10 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+    return NextResponse.next();
+}
+
 export const config = {
-    matcher: [],
+    matcher: [], // Пустой matcher - middleware не будет запускаться
 };
